9b16ec26f13d9d334b766067b5cff37d42c45bf4
김종진 <martia0118@gmail.com>
2021-01-28 00:49:52 +0900

    스트라이크, 볼 갯수 입력하는 것을 추가를 해 두었다.

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -34,7 +34,33 @@ public class MainDrive {
 			// myNumbers의 2번 칸 : 입력값의 1의 자리 = > 4 / 10 % 10
 			myNumbers[2] = input % 10;
 					
+			//얻어 내고 싶은 것은 : 스트라이크 ?개 , 볼, ?개
+			int strikeCount = 0;
+			int ballCount = 0;
+			
+			// ?s ?b인지 판단하는 로직을 작성 = > 내 숫자 들고/ 컴퓨터 숫자와 대조 for문 중첩
+			
+			for (int i = 0; i < myNumbers.length; i++) {
 
+				for (int j = 0; j < cpuNumbers.length; j++) {
+						
+			//내 번흐의 i번째 와 컴퓨터 숫자의 j번째가 같은가?
+					 if(myNumbers[i] == cpuNumbers[j]) {
+						 
+			//숫자가 같다는 건 OK - > 위치도 같은가?
+						 
+						 if(i == j) {
+			//숫자도 같고,위치도 같다 => s찾았따
+							 strikeCount++;
+						 }
+						 
+						 else {
+			//숫자는 같지만, 위치는 다르다 => B찾았다
+							 ballCount++;
+						 }
+					 }
+				}
+			}
 		}
 
 	}
