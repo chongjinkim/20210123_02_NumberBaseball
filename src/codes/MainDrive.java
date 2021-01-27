a30d3087bd79a856e0e8585294cb530b99684e34
김종진 <martia0118@gmail.com>
2021-01-28 00:18:39 +0900

    컴퓨터 문제출력_사용자 3자리 입력

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,10 +1,32 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		//컴퓨터가 3자리 숫자를 출제
+		//객체지향을 배운후에는 랜덤 3자리 문제로 변경
+		
+		int[] cpuNumbers = {7, 4, 1};
+		
+		//스캐너는 한번만 만들어 두면 되니, while앞에서 만들자
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		//3S를 맞출 때 까지 입력을 해서 반복을 받자
+		
+		while(true){
+			
+			System.out.print("3자리 숫자 입력 : ");
+			int input = scan.nextInt();
+			
+			
+		}
+		
+		
 	}
 
 }
