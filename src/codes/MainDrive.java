e4af79de0dec60733be793657be0aeffe2813d3d
김종진 <martia0118@gmail.com>
2021-01-28 00:57:36 +0900

    배열 위치 조정

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -9,7 +9,7 @@ public class MainDrive {
 		// 컴퓨터가 3자리 숫자를 출제
 		// 객체지향을 배운후에는 랜덤 3자리 문제로 변경
 
-		int[] cpuNumbers = { 7, 4, 1 };
+		int[] cpuNumbers = {7, 4, 1};
 
 		// 스캐너는 한번만 만들어 두면 되니, while앞에서 만들자
 
